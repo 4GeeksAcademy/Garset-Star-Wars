c1e9ba4c56161088e0da416edc3aae8b60e4ca82
Elena <pitufa247@gmail.com>
2025-06-16 05:40:27 +0000

    agregue boton de favoritos falta q funcione

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ export const Home = () => {
 			<div className="container mt-5" bg-black  >
 				<>
 					<h1 className="text-center mb-4 text-warning">Star Wars Characters</h1>
-					{store.character.length == 0 ? <div className="text-center mt-5">Loading...</div> :
+					{store.character.length == 0 ? <div className="text-center mt-5 text-warning">Loading...</div> :
 						<div className="d-flex overflow-auto pb-3" style={{ gap: '1rem' }}>
 							{store.character.map((character) => {
 								const id = extractIdFromUrl(character.url);
@@ -51,12 +51,21 @@ export const Home = () => {
 											/>
 											<div className="card-body">
 												<h5 className="card-title">{character.name}</h5>
+												<div className="d-flex align-items-center">
+												<button
+													className="btn btn-outline-warning btn-sm"
+													onClick={() => addFavorite({ id, name: character.name, type: 'character' })}
+												>
+													⚔️ Add
+												</button>
+												<div className="mx-5"></div>
 												<button
 													className="btn btn-warning btn-sm"
-													onClick={() => navigate(`/characters/${id}`)}    
+													onClick={() => navigate(`/characters/${id}`)}
 												>
 													Details
 												</button>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -67,7 +76,7 @@ export const Home = () => {
 
 				<>
 					<h1 className="text-center mb-4 text-warning">Star Wars Planets</h1>
-					{store.planet.length == 0 ? <div className="text-center mt-5">Loading...</div> :
+					{store.planet.length == 0 ? <div className="text-center mt-5 text-warning">Loading...</div> :
 						<div className="d-flex overflow-auto pb-3" style={{ gap: '1rem' }}>
 							{store.planet.map((planet) => {
 								const id = extractIdFromUrl(planet.url);
@@ -86,12 +95,21 @@ export const Home = () => {
 											/>
 											<div className="card-body">
 												<h5 className="card-title">{planet.name}</h5>
-												<button
-													className="btn btn-warning btn-sm"
-													onClick={() => navigate(`planets/${id}`)}
-												>
-													Details
-												</button>
+												<div className="d-flex align-items-center">
+													<button
+														className="btn btn-outline-warning btn-sm"
+														onClick={() => addFavorite({ id, name: planet.name, type: 'planet' })}
+													>
+														🪐 Add
+													</button>
+													<div className="mx-5"></div>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => navigate(`planets/${id}`)}
+													>
+														Details
+													</button>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -102,7 +120,7 @@ export const Home = () => {
 
 				<>
 					<h1 className="text-center mb-4 text-warning">Star Wars Vehicles</h1>
-					{store.vehicle.length == 0 ? <div className="text-center mt-5">Loading...</div> :
+					{store.vehicle.length == 0 ? <div className="text-center mt-5 text-warning">Loading...</div> :
 						<div className="d-flex overflow-auto pb-3" style={{ gap: '1rem' }}>
 							{store.vehicle.map((vehicle) => {
 								const id = extractIdFromUrl(vehicle.url);
@@ -120,12 +138,21 @@ export const Home = () => {
 											/>
 											<div className="card-body">
 												<h5 className="card-title">{vehicle.name}</h5>
-												<button
-													className="btn btn-warning btn-sm"
-													onClick={() => navigate(`/vehicles/${id}`)}
-												>
-													Details
-												</button>
+												<div className="d-flex align-items-center">
+													<button
+														className="btn btn-outline-warning btn-sm"
+														onClick={() => addFavorite({ id, name: vehicle.name, type: 'vehicle' })}
+													>
+														🚀 Add
+													</button>
+													<div className="mx-5"></div>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => navigate(`/vehicles/${id}`)}
+													>
+														Details
+													</button>
+												</div>
 											</div>
 										</div>
 									</div>
